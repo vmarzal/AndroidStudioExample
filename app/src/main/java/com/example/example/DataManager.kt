21d1dce2205437e985b2cd593490622462481f86
Victor <victor.marzal@sistemasgenomicos.com>
2020-02-09 21:07:42 +0100

    second commit

---
+++ app/src/main/java/com/example/example/DataManager.kt
@@ -0,0 +1,27 @@
+package com.example.example
+
+class DataManager {
+
+    private val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses() {
+        var course = CourseInfo("android_intents", "Android Programming with Intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(title = "Java Fundamentals: the Java Language", courseId = "java_lang")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("java_code", "Java Fundamentals: The Core Platform")
+        courses.set(course.courseId, course)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/example/NoteKeeperData.kt
@@ -0,0 +1,6 @@
+package com.example.example
+
+class CourseInfo(val courseId: String, val title: String)
+
+class NoteInfo(var course: CourseInfo, var title: String, var text: String)
+
